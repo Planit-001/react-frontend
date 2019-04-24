42d9ea6837ce4d7939f674359062cfc636a29d4a
Chris <dedles@protonmail.com>
2019-04-24 00:43:03 -0400

    due date can update

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import DeleteIcon from '@material-ui/icons/Delete';
-import AccessTimeIcon from '@material-ui/icons/AccessTime';
-import Tooltip from '@material-ui/core/Tooltip';
 import MomentUtils from '@date-io/moment';
-
 import { MuiPickersUtilsProvider, TimePicker, DatePicker } from 'material-ui-pickers';
+import { connect } from "react-redux";
+import { updateTodo, deleteTodo } from "../redux/actions/index";
+import moment from 'moment';
+
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import DeleteIcon from '@material-ui/icons/Delete';
+import ClearIcon from '@material-ui/icons/Clear';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogActions from '@material-ui/core/DialogActions';
-import TextField from '@material-ui/core/TextField';
-import { connect } from "react-redux";
-import { updateTodo, deleteTodo } from "../redux/actions/index";
-import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
 import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import TextField from '@material-ui/core/TextField';
+import Tooltip from '@material-ui/core/Tooltip';
 
 class TodoItem extends React.Component {
 
@@ -27,18 +30,29 @@ class TodoItem extends React.Component {
     timeModal: false,
     editable: false,
     newTitle: '',
-    selectedDate: new Date(),
+    selectedDate: moment().format(),
+    selectedTime: null
   }
 
   handleDateChange = date => {
+    console.log('date: ', date)
     this.setState({ selectedDate: date });
   };
 
-  updateTodo = (e, id) => {
+  handleTimeChange = time => {
+    console.log('time: ', time)
+    this.setState({ selectedTime: time });
+  };
+
+  updateTodoDone = (e, id) => {
     if(e.target.checked !== undefined){
-      const todoBody = {todo: {done: e.target.checked}};
-      this.props.updateTodo(id, todoBody);
+      this.updateTodo({done: e.target.checked}, id)
     }
+  }
+
+  updateTodo = (todoObject, id) => {
+    const todoBody = { todo: todoObject}
+    this.props.updateTodo(id, todoBody)
   }
 
   updateTodoTitle = (newTitle, id) => {
@@ -55,6 +69,18 @@ class TodoItem extends React.Component {
       timeModal: false
     });
   };
+
+  handleSave = (id) => {
+    this.setState({
+      timeModal: false
+    }, () => {
+      let updateBody = {};
+      if(moment.isMoment(this.state.selectedDate)){
+        updateBody.due_date = this.state.selectedDate.format()
+        this.updateTodo(updateBody, id)
+      }
+    });
+  }
 
   onTodoClick = () => {
     this.setState({
@@ -79,32 +105,39 @@ class TodoItem extends React.Component {
 
   render() {
     const { todo } = this.props;
-    const { selectedDate } = this.state;
+    const { selectedDate, selectedTime } = this.state;
 
     return (
         <ListItem>
           <Dialog
             onClose={this.handleClose}
-            aria-labelledby="customized-dialog-title"
             open={this.state.timeModal}>
             <DialogTitle id="customized-dialog-title" onClose={this.handleClose}>
-              Modal title
+              Choose a Date for the Todo
             </DialogTitle>
             <DialogContent>
               <MuiPickersUtilsProvider utils={MomentUtils}>
 
-                <Grid container justify="space-around">
-                  <DatePicker
-                    margin="normal"
-                    label="Date picker"
-                    value={selectedDate}
-                    onChange={this.handleDateChange}/>
-                    
-                  <TimePicker
-                    margin="normal"
-                    label="Time picker"
-                    value={selectedDate}
-                    onChange={this.handleDateChange}/>
+                <Grid container justify="space-between" >
+                  {/* <Grid item sm={6}> */}
+                    <DatePicker
+                      margin="normal"
+                      disablePast={true}
+                      label="Date picker"
+                      value={selectedDate}
+                      clearable={true}
+                      onChange={this.handleDateChange}/>
+                  {/* </Grid> */}
+                  {/* <Grid item sm={6}>
+                    <TimePicker
+                      margin="normal"
+                      disabled={this.state.selectedDate === null}
+                      clearable={true}
+                      helperText="(optional)"
+                      label="Time picker"
+                      value={selectedTime}
+                      onChange={this.handleTimeChange}/>
+                  </Grid> */}
                 </Grid>
 
               </MuiPickersUtilsProvider>
@@ -113,7 +146,7 @@ class TodoItem extends React.Component {
               <Button onClick={this.handleClose} color="secondary">
                 Cancel
               </Button>
-              <Button onClick={this.handleClose} color="primary">
+              <Button onClick={() => this.handleSave(todo.id)} color="primary">
                 Save changes
               </Button>
             </DialogActions>
@@ -122,7 +155,7 @@ class TodoItem extends React.Component {
           <Checkbox
               checked={todo.done}
               tabIndex={-1}
-              onClick={(e) => this.updateTodo(e, todo.id)}
+              onClick={(e) => this.updateTodoDone(e, todo.id)}
               disableRipple />
           {this.state.editable ? (
             <ClickAwayListener onClickAway={this.handleClickAway}>
