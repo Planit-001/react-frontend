54392214027a5e72181c307956af1ecdbdf0a957
Chris <dedles@protonmail.com>
2019-05-03 14:36:27 -0400

    add build folder

---
+++ build/precache-manifest.1e1c6e16cbcb034c46e505ec4ff07c98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "6adf886f7d4531e180c8",
+    "url": "/static/js/main.1ae6d732.chunk.js"
+  },
+  {
+    "revision": "27f59fb675137693db02",
+    "url": "/static/js/2.5b1f970c.chunk.js"
+  },
+  {
+    "revision": "6adf886f7d4531e180c8",
+    "url": "/static/css/main.094fdc3e.chunk.css"
+  },
+  {
+    "revision": "27f59fb675137693db02",
+    "url": "/static/css/2.b977a83b.chunk.css"
+  },
+  {
+    "revision": "51c98f8ab5cb71382756e8adc2e23be7",
+    "url": "/index.html"
+  }
+];
