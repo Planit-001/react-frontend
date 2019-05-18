232631f07ea847bae9ac932efb533a443dd28520
Chris <dedles@protonmail.com>
2019-05-18 17:02:19 -0400

    extract into new component createListItem

---
+++ src/components/list/CreateListItem.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import TextField from '@material-ui/core/TextField';
+import { connect } from 'react-redux';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import Button from '@material-ui/core/Button';
+
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import ListItemText from '@material-ui/core/ListItemText';
+
+import { createListItem } from "./../../redux/actions/list";
+
+
+const CreateListItem = React.memo(({listId, dispatch }) => {
+    const [createable, setCreatable] = useState(false);
+    const [listItem, setListItem] = useState('');
+  
+    const handleClickAway = () => {
+        setCreatable(false);
+    }
+
+    const onEnter = (e) => {
+        if (e.key === 'Enter' && !(e.target.value === '')) {
+           handleCreate()
+        }
+    }
+
+    const handleCreate = () => {
+        if(listItem){
+            const listItemBody = {
+                title: listItem,
+            };
+        
+            console.log('create list item');
+            dispatch(createListItem(listId, listItemBody))
+        }
+    }
+
+
+   return (
+        <ListItem button disableRipple onClick={() => setCreatable(true)}>
+            <ListItemIcon>
+                <AddCircleOutlineIcon />
+            </ListItemIcon>
+
+            {createable ? (
+                <ClickAwayListener onClickAway={handleClickAway}>
+                    <div style={{width: '90%'}}>
+                        <TextField
+                            label="Add list item"
+                            onKeyPress={onEnter}
+                            fullWidth
+                            autoFocus
+                            style={{width: '100%', paddingRight: 50}}
+                            margin="dense"
+                            value={listItem}
+                            onChange={(e) => setListItem( e.target.value )}/>
+                        <ListItemSecondaryAction>
+                            <Button variant="contained" color="primary" onClick={handleCreate}>
+                                Add
+                            </Button>
+                        </ListItemSecondaryAction>
+                    </div>
+                </ClickAwayListener>
+            ):(
+                <ListItemText primary="Add list item" />
+            )}
+        </ListItem>
+   )
+  });
+  
+  export default connect()(CreateListItem);
--- src/components/list/ListContainer.js
+++ src/components/list/ListContainer.js
@@ -1,35 +1,16 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
-
-// import CreateTodo from './../components/CreateTodo';
-// import TodoList from './../components/TodoList';
-
 import ComponentTitle from './../ComponentTitle';
 import { connect } from "react-redux";
 import { updateList, deleteList, createListItem } from "./../../redux/actions/list";
 import Grid from '@material-ui/core/Grid';
 import Paper from "@material-ui/core/Paper";
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import TextField from '@material-ui/core/TextField';
-
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import Button from '@material-ui/core/Button';
-
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
-
-import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
+import CreateListItem from './CreateListItem';
 
 
 class ListContainer extends React.Component {
 
-    state = {
-        createable: false,
-        newListItem: '',
-    }
   
   updateList = (e, id) => {
 
@@ -38,24 +19,6 @@ class ListContainer extends React.Component {
   deleteList = (id) => {
     this.props.deleteList(id);
   }
-
-  createListItem = () => {
-    const { list } = this.props;
-    const title = this.state.newListItem;
-    const listItemBody = {
-        title,
-    };
-
-    this.props.createListItem(list.id, listItemBody)
-    
-  }
-
-  onEnter = (e) => {
-    if (e.key === 'Enter' && !(e.target.value === '')) {
-        this.handleClickAway()
-    }  
-  }
-
 
   updateListItem = (e, id) => {
     // if(e.target.checked !== undefined){
@@ -68,16 +31,9 @@ class ListContainer extends React.Component {
     // this.props.deleteList(id);
   }
 
-  handleClickAway = () => {
-    this.setState({
-      createable: false
-    })
-  }
-
 
   render() {
     const { list } = this.props;
-    const { createable } = this.state;
 
     return (
       
@@ -87,33 +43,7 @@ class ListContainer extends React.Component {
                 <ComponentTitle title={list.title} />
                 <List>
                     <Divider />
-                    <ListItem button disableRipple onClick={() => this.setState({createable: true})}>
-                        <ListItemIcon>
-                            <AddCircleOutlineIcon />
-                        </ListItemIcon>
-                        
-                        {createable ? (
-                            <ClickAwayListener onClickAway={this.handleClickAway}>
-                                <div style={{width: '90%'}}>
-                                    <TextField
-                                        label="Add list item"
-                                        onKeyPress={this.onEnter}
-                                        fullWidth
-                                        style={{width: '100%', paddingRight: 50}}
-                                        margin="dense"
-                                        value={this.state.newListItem}
-                                        onChange={(e) => this.setState({newListItem: e.target.value})}/>
-                                    <ListItemSecondaryAction>
-                                        <Button variant="contained" color="primary" onClick={this.createListItem}>
-                                            Add
-                                        </Button>
-                                    </ListItemSecondaryAction>
-                                </div>
-                            </ClickAwayListener>
-                        ):(
-                            <ListItemText primary="Add list item" />
-                        )}
-                    </ListItem>
+                    <CreateListItem listId={list.id} />
                 </List>
 
           </Grid>
