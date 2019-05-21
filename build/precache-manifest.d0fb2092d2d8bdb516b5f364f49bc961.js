945fc78dca8963cc2080839ccf11eebc0b644f42
Chris <dedles@protonmail.com>
2019-05-21 01:35:53 -0400

    new build part 2

---
+++ build/precache-manifest.d0fb2092d2d8bdb516b5f364f49bc961.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "a1e913138817be2c7648",
+    "url": "/static/js/main.75555527.chunk.js"
+  },
+  {
+    "revision": "0e6b05c31c0d11f07c37",
+    "url": "/static/js/2.6ebde07c.chunk.js"
+  },
+  {
+    "revision": "a1e913138817be2c7648",
+    "url": "/static/css/main.535c41d1.chunk.css"
+  },
+  {
+    "revision": "0e6b05c31c0d11f07c37",
+    "url": "/static/css/2.bbaf6b7b.chunk.css"
+  },
+  {
+    "revision": "329069e419a23a88033024f31871f1f3",
+    "url": "/index.html"
+  }
+];
