f427ffd3a07cf79aa65b24ed55881be88c705f66
Chris <dedles@protonmail.com>
2019-06-29 00:52:32 -0400

    upgrade infobox

--- src/components/calendar/InfoBox.js
+++ src/components/calendar/InfoBox.js
@@ -5,14 +5,51 @@ import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import {readableDateRange, readableTimeRange} from './../../utils/dateFuncs';
+import moment from 'moment';
+import AddToCalendarHOC from 'react-add-to-calendar-hoc';
+import Icon from '@material-ui/core/Icon';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const dropdownStyles = {
+  padding: "10px",
+  border: "1px solid #E5E5E5",
+  borderTop: "none",
+  width: "300px",
+  backgroundColor: "#FFF",
+  margin: "0 auto",
+};
+
+function Dropdown({ children }) {
+  return (
+    <div style={dropdownStyles}>
+      {children.map((child, index) => (
+          <div key={index}>
+              {child}
+          </div>
+      ))}
+    </div>
+  );
+}
 
 function InfoBox({eventId, eventTitle, eventStart, eventEnd, eventDescription, onUpdate, onDelete}){
-    // if(ics){
-        // var cal = ics();
-        // cal.addEvent('Demo Event', 'This is an all day event', 'Nome, AK', '8/7/2013', '8/7/2013');
-        // console.log('yes')
-    // }
-	// cal.addEvent('Demo Event', 'This is thirty minute event', 'Nome, AK', '8/7/2013 5:30 pm', '8/7/2013 6:00 pm');
+    console.log(eventStart)
+    const event = () => {
+        const startDatetime = moment(eventStart);
+        const endDatetime = moment(eventEnd) //startDatetime.clone().add(2, 'hours');
+        const duration = moment.duration(endDatetime.diff(startDatetime)).asHours();
+        const event = {
+          description: eventDescription,
+          duration: String(duration),
+          endDatetime: endDatetime.format('YYYYMMDDTHHmmssZ'),
+          startDatetime: startDatetime.format('YYYYMMDDTHHmmssZ'),
+          title: eventTitle,
+        }
+        return event;      
+    }
+
+    const AddToCalendarDropdown = AddToCalendarHOC(Button, Dropdown);
 
     return (
         <Card>
@@ -38,12 +75,25 @@ function InfoBox({eventId, eventTitle, eventStart, eventEnd, eventDescription, o
                 {/* <a href="javascript:cal.download()">Demo</a> */}
 
                 {eventId && <CardActions>
+                    
                     <Button onClick={onDelete} size="small" color="secondary">
                         Delete
                     </Button>
                     <Button onClick={onUpdate} size="small" color="default">
                         Edit
                     </Button>
+                    <AddToCalendarDropdown
+                        buttonProps={{
+                            size: 'small',
+                            color: "primary"
+                        }}
+                        buttonText="Download"
+                        // className={componentStyles}
+                        // linkProps={{
+                        //     className: linkStyles,
+                        // }}
+                        event={event()}
+                    />
                 </CardActions>}
                     
                 
